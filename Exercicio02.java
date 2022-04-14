1c76884d5f7e898702c13cb2c4c0c2fb55389464
Júlio César <101370931+JulioBastianelle@users.noreply.github.com>
2022-04-13 21:26:56 -0300

    Update Exercicio02.java

--- Exercicio02.java
+++ Exercicio02.java
@@ -9,12 +9,12 @@ public class Exercicio02 {
 
     public static void main(String[] args) {
         Integer[] numeros = new Integer[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
-       int i = 0;
+        int i = 0;
         while (i < 20) {
-            Integer numerosDivisorios = numeros[i]%3;
-            boolean numerosDividosPorTres = numerosDivisorios == 0;
-            if (numerosDividosPorTres){
-                System.out.println(numeros[i] + " é divisivel por 3!");
+            Integer numero = numeros[i];
+            Integer numerosDivisiveisPorTres = numero % 3;
+            if (numerosDivisiveisPorTres.equals(0)){
+                System.out.println("Numero " + numeros[i] + " é divisivel por 3!");
             }
             i++;
         }
